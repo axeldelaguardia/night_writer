f9c1d612186d3364c4f7eadc3e9baf77d4aa2f0b
Axel De La Guardia <axeldelaguardia@gmail.com>
2023-01-13 13:05:18 -0700

    Add: class method #create_file

--- lib/to_braille.rb
+++ lib/to_braille.rb
@@ -1,10 +1,10 @@
 class ToBraille
-	attr_reader :incoming_text, :en_to_braille_dictionary
+	attr_reader :incoming_text, :outgoing_file, :en_to_braille_dictionary
 	
 	def initialize(terminal_arguments)
-		@incoming_text = File.read(terminal_arguments[0])
-		@outgoing_file = File.new(terminal_arguments[1], 'w')
-
+		@incoming_text = File.read("./io_files/#{terminal_arguments[0]}")
+		@outgoing_file = File.new("./io_files/#{terminal_arguments[1]}", 'w')
+		puts "Created '#{terminal_arguments[1]}' containing #{incoming_text.length} characters"
 		@en_to_braille_dictionary = {
 			'a' => '100000',
 			'b' => '110000',
@@ -56,14 +56,23 @@ class ToBraille
 
 	def convert_to_braille(array)
 		message = ''
+		# a1, a2, a3, a4 = [], [], [], []
 		3.times do |n|
 			array.each do |x|
 				x[n + 0].to_s == '1' ? message << "O" : message << "."
 				x[n + 3].to_s == '1' ? message << "O" : message << "."
 			end
+			# require 'pry'; binding.pry
 			message << "\n"
 		end
 		message
+	end
+
+	def self.create_file(terminal_arguments)
+		to_braille = ToBraille.new(terminal_arguments)
+		braille_array = to_braille.convert_str_to_braille_chars(to_braille.incoming_text)
+		new_text = to_braille.convert_to_braille(braille_array)
+		to_braille.outgoing_file.write(new_text)
 	end
 
 end
