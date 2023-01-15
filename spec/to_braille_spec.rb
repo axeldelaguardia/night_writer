fa8a1dfaf5ead6a2ae9d523b61bbb3024bdb0690
Axel De La Guardia <axeldelaguardia@gmail.com>
2023-01-14 18:23:59 -0700

    Fix: typo on argument'

--- spec/to_braille_spec.rb
+++ spec/to_braille_spec.rb
@@ -64,10 +64,10 @@ describe ToBraille do
 		it 'translates an array to a single string and ready to write on a text file' do
 			dots_and_os = ['OOO.OOOOO.', '....O.O.O.', 'O.O.....O.']
 
-			expect(translator.translate_to_txt(dots)).to eq("OOO.OOOOO.\n....O.O.O.\nO.O.....O.")
+			expect(translator.translate_to_txt(dots_and_os)).to eq("OOO.OOOOO.\n....O.O.O.\nO.O.....O.")
 		end
 	end
-	
+
 	# describe '#convert_to_braille' do
 	# 	it 'converts to printable braille' do
 	# 		hi = ["110010", "010100"]
